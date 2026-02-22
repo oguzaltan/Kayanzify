cee69759fd7f01ad4950373ce2e095739b1b17bf
Oguz Altan <ouz.altan@rwth-aachen.de>
2026-02-22 22:06:27 +0100

    connect to Spotify and retrieve profile information

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
